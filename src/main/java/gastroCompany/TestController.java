7bd0ebc42cd7863507dd8b2a1a37d0fb3a5d9175
Samuraj90 <kolasinski.slawomir@gmail.com>
2021-01-01 16:35:46 +0100

    Dodanie plików bootstrapa, elementów statycznych strony oraz samej strony html

---
+++ src/main/java/gastroCompany/TestController.java
@@ -0,0 +1,13 @@
+package gastroCompany;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String get(){
+        return "test";
+    }
+}
